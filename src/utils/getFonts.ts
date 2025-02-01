0b71621b729afc85d350d3cd8f9bb8ad86a2a136
qpsqps123 <qpsqps321@gmail.com>
2025-02-01 23:34:14 +0900

    add fonts file

---
+++ src/utils/getFonts.ts
@@ -0,0 +1,14 @@
+import { Playwrite_VN, Yeon_Sung } from "next/font/google";
+
+export const playwriteVn = Playwrite_VN({
+  weight: "400",
+  variable: "--font-playwrite-vn",
+  display: "swap",
+});
+
+export const yeonSung = Yeon_Sung({
+  weight: "400",
+  variable: "--font-yeon-sung",
+  subsets: ["latin"],
+  display: "swap",
+});
